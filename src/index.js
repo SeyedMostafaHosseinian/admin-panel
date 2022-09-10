60ca2f32ddf6bdb883c65377a0041afa44e86469
SeyedMostafaHosseinian <seyedmostafainsta786@gmail.com>
2022-09-10 12:46:20 +0430

    Create folder structure and install other required packages

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//components
+import App from "./App";
+
+//style
+import "./index.css";
+
+//theming
+import { ThemeProvider } from '@mui/styles';
+import theme from "./theme/theme";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+ <ThemeProvider theme={theme}>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
+ </ThemeProvider> 
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
